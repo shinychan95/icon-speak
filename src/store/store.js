4fbebb9a9e977bde4e122413c64dffde9ad0c9cf
shinychan95 <shinychan95@gmail.com>
2020-05-07 21:15:04 +0900

    same by same

--- src/store/store.js
+++ src/store/store.js
@@ -8,6 +8,8 @@ const SET_MAP = "setMap";
 const SET_STORE_LIST = "setStoreList";
 const ADD_MARKER = "addMarker";
 
+// state를 설정할 때, 입력받는 parameter가 존재하고,
+// action type에 따라 reducer를 통해 state가 업데이트 된다
 const setMap = (map) => {
   return {
     type: SET_MAP,
@@ -22,40 +24,37 @@ const setStoreList = (storeList) => {
   };
 };
 
-const addMarker = (marker, overlay) => {
+const addMarker = (marker) => {
   return {
     type: ADD_MARKER,
-    marker: marker,
-    overlay: overlay
+    marker: marker
   }
 };
 
-
+// state를 변경시키는 reducer
 const reducer = (state = {
-  maskMap: {
+  searchMap: {
     map: null,
     storeList: [],
-    oldMarker: [],
-    oldOverlay: []
+    markers: []
   }
 }, action) => {
   console.log(action);
   switch (action.type) {
-
     case SET_MAP:
       return {
-        ...state,
-        maskMap: {
-          ...state.maskMap,
-          map: action.map
+        ...state,  // ...은 ES6에서 제안된 문법으로 Rest Operator와 Spread Operator로 나뉜다.
+        searchMap: { // Rest Operator는 변수가 너무 많을 때, 나머지를 배열로 리턴하는 것이고
+          ...state.searchMap, // Spread Operator는 배열이라면 원소들을 나열하고, 오브젝트라면 열거할수 있는 프로퍼티들을 나열합니다.
+          map: action.map // 즉, 여기서는 searchMap 이외의 것들(제일 첫 줄), 그리고 searchMap에서 map을 제외한 나머지는 그대로를 의미한다.
         }
       };
 
     case SET_STORE_LIST:
       return {
         ...state,
-        maskMap: {
-          ...state.maskMap,
+        searchMap: {
+          ...state.searchMap,
           storeList: action.storeList
         }
       };
@@ -64,10 +63,9 @@ const reducer = (state = {
       console.log("adding... :",action);
       return {
         ...state,
-        maskMap: {
-          ...state.maskMap,
-          oldMarker: action.marker,
-          oldOverlay: action.overlay
+        searchMap: {
+          ...state.searchMap,
+          markers: action.marker,
         }
       };
 
--- src/views/MapSearch.js
+++ src/views/MapSearch.js
@@ -1,148 +1,31 @@
-import React, { useEffect } from 'react';
-import { Container, Row, Col, Button } from "reactstrap";
-import axios from 'axios'; // HTTP 클라이언트 라이브러리
-import { Link } from "react-router-dom";
-import Btn from "../component/Btn"
-import Users from "../component/Users";
+import React, { useEffect } from "react";
+import KaKaoMap from "../component/kakaoMap/KakaoMap";
+import useGeolocation from "../action/kakaomap/useGeolocation";
+import useCenterChanged from "../action/kakaomap/useCenterChanged";
+import { useSelector } from "react-redux";
 
-const { kakao } = window
 
-class MapSearch extends React.Component {
+const MapSearch = () => {
+  const { map } = useSelector(state => ({ map: state.maskMap.map }), []);
 
-  componentDidMount() {
-    document.body.classList.add("bg-instagram");
-
-    var keyword = this.props.match.params.place
-    let container = document.getElementById('map');
-    let options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-      level: 4
-    };
-
-    let map = new window.kakao.maps.Map(container, options);
-
-    let locPosition;
-
-    var marker = new kakao.maps.Marker();
-    
-    marker.setMap(map);
-
-    var iwContent = '<div style="padding:5px;">내 위치</div>' // 인포윈도우에 표시할 내용
-    
-    // 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({
-      content : iwContent
-    });
+  const { getGeo } = useGeolocation();
+  const { setEvent } = useCenterChanged();
+  useEffect(() => {
+    getGeo();
+    setEvent();
+  }, [map]);
   
-    // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-    infowindow.open(map, marker); 
-
-    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-    if (navigator.geolocation) {
-        
-      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      navigator.geolocation.getCurrentPosition(function(position) {
-          
-          var lat = position.coords.latitude, // 위도
-              lon = position.coords.longitude; // 경도
-          
-          locPosition = new kakao.maps.LatLng(lat, lon) // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-          map.setCenter(locPosition); 
-          marker.setPosition(locPosition)
-          
-      });
-              
-    } 
-
-   
-    // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
-    kakao.maps.event.addListener(map, 'center_changed', function() {
-      
-      // 지도의 중심좌표를 얻어옵니다 
-      locPosition = map.getCenter(); 
-
-      marker.setPosition(locPosition)
-
-      // 인포윈도우를 마커위에 표시합니다 
-      infowindow.open(map, marker);
-      
-    });
-
-    
-    
-  }
-
-  getSearchResult() {
-
-    axios.post(`https://dapi.kakao.com/v2/local/search/keyword.json?query={s}`, {
-      headers: {
-        'Authorization': 'KakaoAK 2dde8f0dbf6d40840e4a727cd92aab7a'
-      },
-      params: {
-        x: this.locPosition.getLng(),
-        y: this.locPosition.getLat(),
-        radius: 1000,
-        query: this.keyword
-      }
-    })
-      .then(res => {
-        console.log(res)
-    })
-    .catch(e => {
-      console.log("error", e)
-      console.log("chanyoung")
-    })
-  }
-
-  handleClick() {
-    console.log("chan")
-    this.getResult()
-  }
-
-  render() {
-    return (
-      <>
-        <div className="main-content">
-            <div className="header bg-instagram pb-1 py-3 py-lg-6">
-              <Container className="pb-1 text-center">
-                <div className="header-body text-center">
-								  <h1 className="my-4 text-black">Icon Speak</h1>
-                </div>
-              </Container>
-            </div>
-            <div className="body bg-instagram pb-9">
-              <Container className="pb-1 text-center">
-                <Col className="justify-content-center">
-                  <div id="map" style={{margin: "auto", maxWidth: "100%", height: "400px"}} />
-                  <Link to="/select_Location">
-                    <Button className="my-1 mt-4" color="primary" type="button">
-                      장소 탐색
-                    </Button>                  
-                  </Link>
-                </Col>
-                <Col>
-                  <Button className="my-1 mt-1" color="primary" type="button" onClick={this.getSearchResult()}>
-                      검색
-                  </Button>
-                </Col>
-                <Col>
-                  <Users></Users>
-                </Col>
-                <Col>
-                  <Btn></Btn>
-                </Col>
-                <Col>
-                  <Link to="/select_place">
-                    <Button color="secondary" type="button">뒤로가기</Button>
-                  </Link>
-                </Col>
-              </Container>                 
-            </div>
-        </div>
-      </>
-    );
-  }
+  return <div>
+    <div>
+      <h3>공적 마스크 지도 입니다.</h3>
+      <h4>해당 마커를 클릭 하시면 자세한 정보를 확인 할 수 있습니다.</h4>
+      <h4>* 실제정보와 5분정도의 시간차이가 있습니다.</h4>
+    </div>
+    <div style={{ height: "50vh", paddingLeft: "10px", paddingRight: "10px" }}>
+      <KaKaoMap >
+      </KaKaoMap>
+    </div>
+  </div>
 }
-
 
 export default MapSearch;
